229b77bfa9324dfce9583dac2fbe51a099a4f4c3
amitavainfluxiq <amitava.involutiontech@gmail.com>
2019-07-29 12:38:57 +0530

    first commioioioot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,38 @@ import { AppComponent } from './app.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { MetaModule } from '@ngx-meta/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
+
+import {DemoMaterialModule} from '../app/material-module';
+
+import { HttpClientModule } from '@angular/common/http';
+import {  ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+
+import {ListingModule} from "listing-angular7";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutusComponent,
-    ContactusComponent
+    ContactusComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
+    DemoMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ListingModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule,MetaModule.forRoot()
+    AppRoutingModule,MetaModule.forRoot(), BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
